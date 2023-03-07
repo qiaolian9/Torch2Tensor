835afd8785c0c4b9d945612fdbd2c5f6578ede76
qiaolian9 <1018718418@qq.com>
2023-03-07 22:20:51 +0800

    update __init__.py

--- Torch2Tensor/__init__.py
+++ Torch2Tensor/__init__.py
@@ -1,12 +1,13 @@
 import os
 import warnings
 
+__version__ = "0.0.1"
 
 if os.path.dirname(os.path.realpath(__file__)) == os.path.join(
-    os.path.realpath(os.getcwd()), "brocolli"
+    os.path.realpath(os.getcwd()), "Torch2Tensor"
 ):
     message = (
-        "You are importing mlc within its own root folder ({}). "
+        "You are importing Torch2Tensor within its own root folder ({}). "
         "This is not expected to work and may give errors. Please exit the "
         "mlc project source and relaunch your python interpreter."
     )
